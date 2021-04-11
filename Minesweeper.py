ebd48b711691e5b0e4bf426a12c58bfb8975ad36
babilal2129 <79623421+babilal2129@users.noreply.github.com>
2021-04-11 23:25:56 +0530

    Update Minesweeper.py

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,154 +1,163 @@
-# This is a simple minesweeper game
-# In this game, 'O' represents Mines
 
 import tkinter as tk
 import random
 
-class Buttons_init:
-    def __init__(self,R,C):
-        self.row = R
-        self.column = C
-        self.button = tk.Button( Game, text=" ", command = self.clicked,bg = "grey", 
-                                 activebackground = "white", height=1, width=1)
-        self.button.grid(row=self.row, column=self.column)
-buttons[self.row,self.column]=self.button
+class Cell:
+	def __init__(self,R,C,click):
+		self.row = R
+		self.column = C
+		self.click = click
+	def Button(self):
+		self.button = tk.Button( Game, text='', command = lambda:self.click(self.row,self.column), activebackground='white', height=1, width=1)
+		self.button.grid(row=self.row, column=self.column)
+		return self.button
+
+class game_play:
+	def __init__(self, data,clusters, size):
+		self.size = size
+		self.data = data
+		self.clusters = clusters
+		self.game_board()
+		self.first_hint()
 	
-    def clicked( self ):
-        if(game_data[ self.row, self.column ]=="O"):    # if there is Mine, Game over
-            buttons[self.row, self.column].configure(activebackground="red",bg="red" )
-            game_over()
-        # if empty cell, clear neighbour empty cells
-        if(game_data[ self.row, self.column ]==" "):
-            clear_blocks(self.row, self.column)
-        else:
-            buttons[self.row, self.column].configure(text=game_data[ self.row, self.column ])
-            buttons[self.row, self.column].configure(bg = "white")
-    # this function starts when Mine is clicked
-    def game_over():
-        for  i in range( 23 ):
-            for  j in range( 8 ):
-                buttons[i,j].configure(text=game_data[i,j])
-                buttons[i,j].configure(bg='white')
+	def game_board(self):
+		self.buttons = []
+		click=self.clicked
+		for  i in range( self.size ):
+			tmp = []
+			for  j in range( self.size ):
+				c = Cell( i, j, click )
+				tmp.append( c.Button() )
+			self.buttons.append(tmp)
+	
+	def clicked( self,R,C ):
+		self.row = R
+		self.column = C
+		if(self.data[self.row][self.column] == 'X'):
+			self.buttons[self.row][self.column].configure(activebackground='red' )
+			self.game_over()
+		elif(self.data[ self.row][self.column ]==' '):
+			self.clear_cluster()
+		else:
+			self.buttons[self.row][self.column].configure(fg='blue', bg='white', text=self.data[self.row][self.column])
 
-# this function clears neighbouring empty cells when an empty cell is clicked
-def clear_blocks(r,c):
-    for block in empty_blocks.values():
-        if ((r,c) in block):
-            for (rr,cc) in block:
-                buttons[rr,cc].configure(text=game_data[rr,cc])
-                buttons[rr,cc].configure(bg = "white")
-            break
-
-def counter( R,C ):
-    rl = start( R, 22)
-    cl = start( C, 7 )
-    count = 0
-    for r in rl:
-        for c in cl:
-            if (game_data[r,c] == 'O'):
-                count += 1
-    return count
-
-def start(rc,lm):
-    if (rc == 0):
-        x=[rc,rc+1]
-    elif(rc == lm):
-        x=[rc-1,rc]
-    else:
-        x=[rc-1,rc,rc+1]
-    return (x)
-
-def empty_blocks_func(er,ec):
-    flag=False
-    global num
-    if((empty_blocks != {})):
-        rl = start( er,22)
-        cl = start( ec,7)
-        for rr in rl:
-            for cc in cl:
-                if (empty[rr,cc] == "x"):
-                    for block in empty_blocks.values():
-                        if((rr,cc) in block):
-                            block.add((er,ec))
-                            flag=True
-    if((empty_blocks=={}) or (flag is False)):
-    empty_blocks[ num ] = set( )
-    empty_blocks[ num ].add((er,ec))
-    num +=1
-
-def merge( ):
-    l=len( empty_blocks )
-    del_list = [ ]
-    for k1 in range(1,l):
-        for k2 in range(k1+1,l+1):
-            if( empty_blocks[k1].intersection(empty_blocks[k2])):
-                if (k2 not in del_list):
-                    empty_blocks[k1].update(empty_blocks[k2])
-                    del_list.append(k2)
-    for key in del_list:
-        del empty_blocks[ key ]
-    empty_b={}
-    for key in empty_blocks.keys():
-        empty_b[key] = set( )
-    for (r,c) in empty_blocks[key]:
-        rl = start(r,22)
-	      cl = start(c,7)
-        for rr in rl:
-            for cc in cl:
-                empty_b[ key ].add((rr,cc))
-    for key in empty_blocks.keys( ):
-        empty_blocks[key].update(empty_b[key])
+	def game_over(self):
+		for  i in range( self.size ):
+			for  j in range( self.size ):
+				if(self.data[i][j] == 'X'):
+					self.buttons[i][j].configure(bg='orange')
+				else:
+					self.buttons[i][j].configure(text=self.data[i][j], bg='white')
+	
+	def clear_cluster(self):
+		for cluster in self.clusters:
+			if ((self.row,self.column) in cluster):
+				for (r,c) in cluster:
+					self.buttons[r][c].configure( fg='blue', bg='white', text=self.data[r][c]  )
+				self.clusters.remove(cluster)
+				break
+	
+	def first_hint(self):
+		cluster = list(random.choices(self.clusters)[0])
+		not_empty =True
+		i = 0
+		while(not_empty):
+			(r,c)=cluster[i]
+			if(self.data[r][c] == ' '):
+				not_empty = False
+			else:
+				i+=1
+		self.buttons[r][c].configure(bg='lightblue')
 
 
-# From here game begins
-# here we initialize game data
+class game_data:
+	def __init__(self, size, no_of_mines):
+		self.create_empty_data(size)
+		self.insert_mines(no_of_mines)
+		self.insert_mine_counts()
+		self.cluster_empty_cells()
+	
+	def create_empty_data(self, size):
+		self.size = size
+		self.data=[ [' ']*self.size for _ in range(self.size) ]
+	
+	def insert_mines(self, no_of_mines):
+		self.mines=[]
+		for mine in random.sample(range(self.size*self.size), no_of_mines):
+			r = mine//self.size
+			c = mine%self.size
+			self.mines.append((r,c))
+			self.data[r][c] = 'X'
+			
+	def insert_mine_counts(self):
+		for r,c in self.mines:
+			for row in self.indices(r):
+				for colmn in self.indices(c):
+					if(self.data[row][colmn]==' '):
+						self.data[row][colmn] = 1
+					elif(self.data[row][colmn]!='X'):
+						self.data[row][colmn] += 1
+
+	def cluster_empty_cells(self):
+		self.clusters = []
+		self.empty = self.empty_cells()
+		for r,C in enumerate(self.empty):
+			for c in C:
+				rows = self.indices(r)
+				columns = self.indices(c)
+				s=set()
+				for i in rows:
+					for j in columns:
+						s.add((i,j))
+				self.clusters.append(s)
+	
+		p1 = 0
+		while(p1<len(self.clusters)):
+			p2 = p1+1
+			not_deleted = True
+			while(p2<len(self.clusters)):
+				intersection = False
+				inter = list(self.clusters[p1].intersection(self.clusters[p2]))
+				for r,c in inter:
+					if(self.data[r][c]==' '):
+						intersection = True
+						break
+				if (intersection):
+					self.clusters[p1].update(self.clusters[p2])
+					self.clusters.pop(p2)
+					not_deleted = False
+				else:
+					p2 += 1
+			if(not_deleted):
+				p1 += 1
+	
+	def empty_cells(self):
+		empty = []
+		for r in range(self.size):
+			column=list(filter( lambda c:self.data[r][c]==' ', range(self.size)))
+			empty.append(column)
+		return empty
+	
+	def indices(self, index):
+		if (index == 0):
+			return (index, index+1)
+		elif(index == self.size-1):
+			return (index-1, index)
+		else:
+			return (index-1, index, index+1)
 
 
-Game = tk.Tk( )
-# random indices are generated where Mines are placed in game data
-mine_index = random.sample(range(8*23),26)    
-game_data = { }   # initialize variable to store game data
-index=0
-
-# Here 'O' which represents Mines are inserted at mine indices
-for i in range(23):
-    for j in range(8):
-        if (index in mine_index) :
-            game_data[i,j] = "O"
-        else:
-            game_data[i,j] = " "
-        index += 1
-
-buttons={ }
-empty={ }
-num = 1
-empty_blocks={ }
-
-# Here, in each cell, a number which represents no of Mines
-# around neighbour cells is stored    
-for  i in range( 23 ):
-    for  j in range( 8 ):
-        if ( game_data[i,j] != "O" ):
-            count=counter( i, j )
-            if(count != 0):
-                game_data[i,j]=str(count)
-                count = 0
-        if (game_data[i,j]==" "):   # empty dict. stores empty cells
-            empty[i,j] = "x"
-        else:
-            empty[i,j] = " "
-
-# Here neighbouring empty cells are grouped
-for  i in range( 23 ):
-    for  j in range( 8 ):
-        if (empty[i,j] == 'x'):
-            empty_blocks_func(i,j)
-
-merge()
-
-# Initializing Buttons
-for  i in range( 23 ):
-    for  j in range( 8 ):
-        Buttons_init( i, j )
-
-Game.mainloop( )
+if __name__ == '__main__':
+	Game = tk.Tk( )
+	Game.title('Minesweeper')
+	
+	size = 10
+	no_of_mines = 16
+	
+	g_data = game_data(size, no_of_mines)
+	data = g_data.data
+	clusters = g_data.clusters
+	
+	game_play(data, clusters, size)
+	
+	Game.mainloop( )
